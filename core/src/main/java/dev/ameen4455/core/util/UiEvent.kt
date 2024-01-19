198c6d749d54b489fa85fbea2bdde9392c301a65
ameen4455 <ameen4455@gmail.com>
2024-01-19 12:45:50 +0530

    Add navigation

---
+++ core/src/main/java/dev/ameen4455/core/util/UiEvent.kt
@@ -0,0 +1,6 @@
+package dev.ameen4455.core.util
+
+sealed class UiEvent {
+    data class Navigate(val route: String): UiEvent()
+    object NavigateUp: UiEvent()
+}
--- onboarding/onboarding_presentation/src/main/java/dev/ameen4455/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/dev/ameen4455/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -15,11 +15,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.ameen4455.core.R
+import dev.ameen4455.core.navigation.Route
+import dev.ameen4455.core.util.UiEvent
 import dev.ameen4455.core_ui.LocalSpacing
 import dev.ameen4455.onboarding_presentation.components.ActionButton
 
 @Composable
-fun WelcomeScreen() {
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit
+) {
     val spacing = LocalSpacing.current
     Column(
         modifier = Modifier
@@ -36,7 +40,9 @@ fun WelcomeScreen() {
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
         ActionButton(
             text = stringResource(id = R.string.next),
-            onClick = { /*TODO*/ },
+            onClick = {
+                      onNavigate(UiEvent.Navigate(Route.AGE))
+            },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
