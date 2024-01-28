4acc13d8595af6df9bd7b2a0bda64cece431db45
ameen4455 <ameen4455@gmail.com>
2024-01-28 16:17:18 +0530

    Added db settings

--- tracker/tracker_data/src/main/java/dev/ameen4455/tracker_data/remote/OpenFoodApi.kt
+++ tracker/tracker_data/src/main/java/dev/ameen4455/tracker_data/remote/OpenFoodApi.kt
@@ -1,5 +1,18 @@
 package dev.ameen4455.tracker_data.remote
 
-interface OpenFoodApi {
+import dev.ameen4455.tracker_data.remote.dto.SearchDto
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface OpenFoodApi {
+    @GET("cgi/search.pl?search_simple=1&json=1&action=process&fields=product_name,nutriments,image_front_thumb_url")
+    suspend fun searchFood(
+        @Query("search_terms") query: String,
+        @Query("page") page: Int,
+        @Query("page_size") pageSize: Int
+    ): SearchDto
+
+    companion object {
+        const val BASE_URL = "https://us.openfoodfacts.org/"
+    }
 }
